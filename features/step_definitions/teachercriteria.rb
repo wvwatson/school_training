363f35bc0e9fcc9aeb4e4c2d4ff2736dec8edc5d
wavell watson <watson@Macintosh-62.local>
2010-12-09 12:12:51 -0600

    added some scenarios, steps, and place to put your code -- schooltraining.rb under lib

---
+++ features/step_definitions/teachercriteria.rb
@@ -0,0 +1,11 @@
+Given /^I have entered "([^"]*)", "([^"]*)", "([^"]*)", "([^"]*)", "([^"]*)" as authorized grades$/ do |arg1, arg2, arg3, arg4, arg5|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I press show criteria$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^the result should be "([^"]*)", "([^"]*)", "([^"]*)", "([^"]*)", "([^"]*)" on the screen$/ do |arg1, arg2, arg3, arg4, arg5|
+  pending # express the regexp above with the code you wish you had
+end
--- features/teachercriteria.feature
+++ features/teachercriteria.feature
@@ -2,4 +2,9 @@ Feature: Teacher Criteria
   In order to outline my background 
   As a teacher
   I want to add my teaching criteria and history
+
+  Scenario: Add some criteria
+    Given I have entered "kindergarten", "1st", "2nd", "3rd", "4th" as authorized grades
+    When I press show criteria
+    Then the result should be "kindergarten", "1st", "2nd", "3rd", "4th" on the screen
   
