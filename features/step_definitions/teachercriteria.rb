110b027d0a52344fcbbf3b1670e8923b26b523bd
Wavell <watson@preferit.com>
2010-12-10 10:05:55 -0600

    added some classes in the rb codefile

--- features/step_definitions/teachercriteria.rb
+++ features/step_definitions/teachercriteria.rb
@@ -1,3 +1,9 @@
+# set up the classes
+Before do
+  @teacher = Teacher.new
+  @authorizedGrades = Criteria.new
+end
+
 Given /^I have entered "([^"]*)", "([^"]*)", "([^"]*)", "([^"]*)", "([^"]*)" as authorized grades$/ do |arg1, arg2, arg3, arg4, arg5|
   pending # express the regexp above with the code you wish you had
 end
--- lib/schooltraining.rb
+++ lib/schooltraining.rb
@@ -1,1 +1,15 @@
 #your code goes in here
+#comments begin with a #
+# before you check anything in run cucumber features
+#  Make sure there isn't any red
+#  You can look at this as making sure things "compile"
+
+class Criteria
+end
+
+class Teacher
+end
+
+class Training
+end
+
